c11f6ea1618b6514bb33f5f7c0e11f7a194f2b1b
Nalinshay Aneja <nalinshay.aneja@sahrecare.com>
2024-11-26 19:34:58 +0530

    Test

---
+++ src/main/java/Data/Configuration.java
@@ -0,0 +1,90 @@
+package Data;
+
+import java.util.Properties;
+
+public class Configuration {
+	public static Properties getMerchantDetails() {
+		Properties props = new Properties();
+
+		// HTTP_Signature = http_signature and JWT = jwt
+		props.setProperty("authenticationType", "http_signature");
+		props.setProperty("merchantID", "testrest");
+		props.setProperty("runEnvironment", "apitest.cybersource.com");
+		props.setProperty("requestJsonPath", "src/main/resources/request.json");
+
+		// MetaKey Parameters
+		props.setProperty("portfolioID", "");
+		props.setProperty("useMetaKey", "false");
+
+		// JWT Parameters
+		props.setProperty("keyAlias", "testrest");
+		props.setProperty("keyPass", "testrest");
+		props.setProperty("keyFileName", "testrest");
+
+		// P12 key path. Enter the folder path where the .p12 file is located.
+
+		props.setProperty("keysDirectory", "src/main/resources");
+		// HTTP Parameters
+		props.setProperty("merchantKeyId", "08c94330-f618-42a3-b09d-e1e43be5efda");
+		props.setProperty("merchantsecretKey", "yBJxy6LjM2TmcPGu+GaJrHtkke25fPpUX+UY6/L/1tE=");
+		// Logging to be enabled or not.
+		props.setProperty("enableLog", "true");
+		// Log directory Path
+		props.setProperty("logDirectory", "log");
+		props.setProperty("logFilename", "cybs");
+
+		// Log file size in KB
+		props.setProperty("logMaximumSize", "5M");
+
+		// OAuth related properties.
+		props.setProperty("enableClientCert", "false");
+		props.setProperty("clientCertDirectory", "src/main/resources");
+		props.setProperty("clientCertFile", "");
+		props.setProperty("clientCertPassword", "");
+		props.setProperty("clientId", "");
+		props.setProperty("clientSecret", "");
+
+		/*
+		PEM Key file path for decoding JWE Response Enter the folder path where the .pem file is located.
+		It is optional property, require adding only during JWE decryption.
+		*/
+		props.setProperty("jwePEMFileDirectory", "src/main/resources/NetworkTokenCert.pem");
+
+		return props;
+
+	}
+	
+	public static Properties getAlternativeMerchantDetails() {
+		Properties props = new Properties();
+
+		// HTTP_Signature = http_signature and JWT = jwt
+		props.setProperty("authenticationType", "http_signature");
+		props.setProperty("merchantID", "testrest_cpctv");
+		props.setProperty("runEnvironment", "apitest.cybersource.com");
+		props.setProperty("requestJsonPath", "src/main/resources/request.json");
+
+		// JWT Parameters
+		props.setProperty("keyAlias", "testrest_cpctv");
+		props.setProperty("keyPass", "testrest_cpctv");
+		props.setProperty("keyFileName", "testrest_cpctv");
+
+		// P12 key path. Enter the folder path where the .p12 file is located.
+
+		props.setProperty("keysDirectory", "src/main/resources");
+		// HTTP Parameters
+		props.setProperty("merchantKeyId", "964f2ecc-96f0-4432-a742-db0b44e6a73a");
+		props.setProperty("merchantsecretKey", "zXKpCqMQPmOR/JRldSlkQUtvvIzOewUVqsUP0sBHpxQ=");
+		// Logging to be enabled or not.
+		props.setProperty("enableLog", "true");
+		// Log directory Path
+		props.setProperty("logDirectory", "log");
+		props.setProperty("logFilename", "cybs");
+
+		// Log file size in KB
+		props.setProperty("logMaximumSize", "5M");
+
+		return props;
+
+	}
+
+}
